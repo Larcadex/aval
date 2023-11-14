cd2a70020ae70bd2052e82d455e65f4789d499d2
серожа <cepbora@vk.com>
2023-11-14 23:17:57 +0300

    Initial commit

---
+++ aval/MainWindow.axaml.cs
@@ -0,0 +1,55 @@
+using System.Globalization;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+
+namespace aval
+{
+    
+    public partial class MainWindow : Window
+    {
+        private int cur_image = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void Button_click(object sender, RoutedEventArgs args)
+        {
+            cur_image = cur_image + 1;
+            if (cur_image == 4) cur_image = 0;
+
+            switch (cur_image)
+            {
+                case 0:
+                    Image0.IsVisible = true;
+                    Image1.IsVisible = false;
+                    Image2.IsVisible = false;
+                    Image3.IsVisible = false;
+                    break;
+                case 1:
+                    Image0.IsVisible = false;
+                    Image1.IsVisible = true;
+                    Image2.IsVisible = false;
+                    Image3.IsVisible = false;
+                    break;
+                case 2:
+                    Image0.IsVisible = false;
+                    Image1.IsVisible = false;
+                    Image2.IsVisible = true;
+                    Image3.IsVisible = false;
+                    break;
+                case 3:
+                    Image0.IsVisible = false;
+                    Image1.IsVisible = false;
+                    Image2.IsVisible = false;
+                    Image3.IsVisible = true;
+                    break;
+            }
+
+            
+        }
+    }
+}
